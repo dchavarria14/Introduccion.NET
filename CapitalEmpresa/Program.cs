f00528a3877452153ba8d2360c90db1e150e8a1f
JADPA-05 <JADPA-05@JADPA-05>
2022-08-04 11:04:07 -0600

    Agregar archivos de proyecto.

---
+++ CapitalEmpresa/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("** CAPITAL DE EMPRESA ** ");
+Console.WriteLine(" ");
+
+double aportacion1, aportacion2, aportacion3;
+
+
+Console.WriteLine("¿Cuanto aporto el socio 1?");
+aportacion1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("¿Cuanto aporto el socio 2?");
+aportacion2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("¿Cuanto aporto el socio 3?");
+aportacion3 = double.Parse(Console.ReadLine());
+
+double capital = aportacion1 + aportacion2 + aportacion3;
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("La capital de la empresa es:" + capital.ToString("0.00"));
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("** CONVERSION DE MEDIDAS ** ");
+Console.WriteLine(" ");
+
+double metros;
+
+
+
+
+Console.WriteLine("Ingrese la medida de telas en metros");
+metros = double.Parse(Console.ReadLine());
+
+double cm = metros * 100.0;
+Console.WriteLine("------------------------------------------------");
+Console.WriteLine("La medida en centimetros es: " +cm.ToString("0.00"));
+
+double pie = metros / 12.0;
+
+Console.WriteLine("La medida en pies es: " +pie.ToString("0.00"));
+
+double yarda = metros / 3.0;
+Console.WriteLine("La medida en yardas es: " +yarda.ToString("0.00"));
+
+double pulgada = metros / 2.54;
+Console.WriteLine("La medida en pulgadas es: "+pulgada.ToString("0.00"));
+
+
+
+
+
